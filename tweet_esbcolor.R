44cf5f5170eb8df8453a8bd7fcea633aa59b653f
Sam Swift <swift@betterment.com>
2015-05-10 12:23:45 -0400

    adding hash tags and checking for tweet char limit

--- tweet_esbcolor.R
+++ tweet_esbcolor.R
@@ -48,6 +48,42 @@ GetColorSchedule <- function(cache.expire.hrs = 12,
   sched[, most_recent_scrape := scraped_datetime == pmax(scraped_datetime),
         by= event_date]
 
+  # try to make a hash tag
+  HashTagDesc <- function(desc){
+
+    inds <- c(str_locate(desc, "in honor of")[2],
+              str_locate(str_to_lower(desc), "anniversary of")[2],
+              str_locate(desc, "in celebration of")[2])
+
+    if(length(na.omit(inds))) {
+      ind.start <- max(na.omit(inds)) + 1
+    } else {
+      return(NULL)
+    }
+
+    desc <- str_replace_all(desc, "'", "’")
+
+    if(!is.na(str_locate(desc, "’s")[2]) &
+       str_locate(desc, "’s")[2] > ind.start) {
+      ind.end <- str_locate(desc, "’s")[2]-2
+    } else {
+      ind.end <- str_length(desc)-1
+    }
+
+    event <- str_sub(desc, ind.start, ind.end)
+    event <- str_replace_all(event, "the ", "")
+    event <- str_replace_all(event, "The ", "")
+    event <- str_replace_all(event, "[[:punct:]]", "")
+    event <- str_trim(event)
+    event <- str_split(event, "and")
+    event <- c(unlist(sapply(event, str_replace_all, " ", "")))
+
+    hash.tag <- paste(str_c("#",event), collapse=" ")
+  }
+
+  sched[, hashtag := HashTagDesc(desc), by=event_date]
+
+  setkey(sched, event_date)
   # save to cachefile
   save(sched,file = kCacheFilename)
   return(sched)
@@ -55,30 +91,45 @@ GetColorSchedule <- function(cache.expire.hrs = 12,
 
 ComposeTweet <- function(tweet.date = Sys.Date()){
   sched <- GetColorSchedule(cache.expire.hrs = 0)
-  sched <- sched[event_date == tweet.date & most_recent_scrape == TRUE]
+  day <- sched[event_date == tweet.date & most_recent_scrape == TRUE]
 
-  if(sched[, .N] > 0){
-    tweet <- sched[, paste(desc, url)]
+  if(day[, .N] > 0){
+
+    # start tweeet with the first 140 char of description
+    tweet <- day[, str_sub(desc, 1,140)]
+
+    # add a hashtag if we have room
+    if(str_length(paste(tweet, day[, hashtag])) <= 140) {
+      tweet <- paste(tweet, day[, hashtag])
+    }
+
+    # add the url (which will be shortened to 22 chars) if we have room
+    if(str_length(tweet) + 22 <= 140){
+      tweet <- paste(tweet, day[, url])
+    }
     return(tweet)
   } else {
     return(NULL)
   }
 }
 
-PostTweet <-function(tweet.date = Sys.Date()){
+PostTweet <-function(tweet.date = Sys.Date(), test=FALSE){
   tweet.text <- ComposeTweet(tweet.date)
 
   if(is.null(tweet.text)){
     return(FALSE)
   } else {
-
     options(httr_oauth_cache=T)
     setup_twitter_oauth(consumer_key = kCreds$consumer_key,
                         consumer_secret = kCreds$consumer_secret,
                         access_token = kCreds$access_token,
                         access_secret = kCreds$access_secret)
 
-    tweet(tweet.text)
+    if(!test){
+      tweet(tweet.text)
+    } else {
+      message(tweet.text)
+    }
   }
 }
 
