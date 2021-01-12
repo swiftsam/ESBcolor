a3527b27836d16e8e86750de2c41f4b5fedc5ec7
Sam Swift <sam@boweryfarming.com>
2021-01-11 22:34:44 -0500

    catch errors and write logs

--- tweet_esbcolor.R
+++ tweet_esbcolor.R
@@ -3,6 +3,8 @@ library(rvest)
 library(magrittr)
 library(twitteR)
 library(stringr)
+library(purrr)
+library(readr)
 
 page <- read_html("http://www.esbnyc.com/explore/tower-lights/calendar")
 
@@ -18,26 +20,43 @@ reason <- page %>%
   html_node("div.today div.info div.field_description p") %>%
   html_text()
 
-log_file_name <- paste0(format(Sys.Date(), "%Y%m%d"),".txt")
+log_file_name <- file.path("tweets", paste0(format(Sys.Date(), "%Y%m%d"),".txt"))
 
 if(!file.exists(log_file_name)){
   if(str_starts(color_date, "Today,")) {
     if(colors != "SIGNATURE WHITE COLOR") {
 
-      tweet_color  <- colors %>% str_to_lower %>% str_remove(" color") %>% str_to_sentence()
-      tweet_reason <- reason
+      tweet_safely <- safely(function() {
 
-      tweet_text   <- paste0(post_color, "\n\n", post_reason)
+        tweet_color  <- colors %>% str_to_lower %>% str_remove(" color") %>% str_to_sentence()
+        tweet_reason <- reason
 
-      options(httr_oauth_cache=T)
-      creds <- read_yaml("ESBcolor.yaml")$esbcolor
+        tweet_text   <- paste0(post_color, "\n\n", post_reason)
 
-      setup_twitter_oauth(consumer_key    = creds$consumer_key,
-                          consumer_secret = creds$consumer_secret,
-                          access_token    = creds$access_token,
-                          access_secret   = creds$access_secret)
-      status <- tweet(tweet_text)
+        creds <- read_yaml("ESBcolor.yaml")$esbcolor
+        options(httr_oauth_cache=T)
+        setup_twitter_oauth(consumer_key    = creds$consumer_key,
+                            consumer_secret = creds$consumer_secret,
+                            access_token    = creds$access_token,
+                            access_secret   = creds$access_secret)
+        tweet(tweet_text)
+
+      })
+
+      response <- tweet_safely()
+
+      if(is.null(response$error)) {
+        write_lines(tweet_text, log_file_name)
+      } else {
+        message(response$error)
+      }
+    } else {
+      message("just white lights")
     }
+  } else {
+    message("didn't find today's info")
   }
+} else {
+  message("already tweeted for today")
 }
 
