a35f5ff9a4e7bd78ed34b69bd4610b280549ae43
Sam Swift <sam@boweryfarming.com>
2021-01-11 22:01:12 -0500

    standardizing file names

--- tweet_esbcolor.R
+++ tweet_esbcolor.R
@@ -4,8 +4,6 @@ library(magrittr)
 library(twitteR)
 library(stringr)
 
-kCreds         <- yaml.load_file("esb.yml")[['esbcolor']]
-
 page <- read_html("http://www.esbnyc.com/explore/tower-lights/calendar")
 
 color_date <- page %>%
@@ -29,15 +27,16 @@ if(!file.exists(log_file_name)){
       tweet_color  <- colors %>% str_to_lower %>% str_remove(" color") %>% str_to_sentence()
       tweet_reason <- reason
 
-      tweet_text   <- paste0(post_color, "\n", post_reason)
+      tweet_text   <- paste0(post_color, "\n\n", post_reason)
 
       options(httr_oauth_cache=T)
-      setup_twitter_oauth(consumer_key = kCreds$consumer_key,
-                          consumer_secret = kCreds$consumer_secret,
-                          access_token = kCreds$access_token,
-                          access_secret = kCreds$access_secret)
-      status <- tweet(tweet_text)
+      creds <- read_yaml("ESBcolor.yaml")$esbcolor
 
+      setup_twitter_oauth(consumer_key    = creds$consumer_key,
+                          consumer_secret = creds$consumer_secret,
+                          access_token    = creds$access_token,
+                          access_secret   = creds$access_secret)
+      status <- tweet(tweet_text)
     }
   }
 }
